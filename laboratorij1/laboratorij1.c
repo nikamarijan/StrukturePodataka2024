982413f68e73c5ee035ec9bc0fe7ba3c667bbac6
nikamarijan <nika.marijan656@gmail.com>
2024-10-08 20:29:39 +0200

    labaratorij1 dodana count_rows funkcija

--- laboratorij1/laboratorij1.c
+++ laboratorij1/laboratorij1.c
@@ -1,8 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<stdlib.h>
+#define ERROR_OPENING_FILE -1
+#define BUFFER_SIZE 1024
+
+int count_rows(char* filename);
 
 int main()
 {
-	printf("Hello world!");
+	int OpenFile(char*);
 
 	return 0;
+}
+int count_rows(char* filename) {
+	FILE* fp = NULL;
+	char buffer[BUFFER_SIZE];
+	int count = 0;
+
+	fp = fopen(filename, "r");
+	if (fp == NULL) {
+		printf("Error opening file\n");
+		return ERROR_OPENING_FILE;
+	}
+	while (!feof(fp)) {
+		fgets(buffer, BUFFER_SIZE, fp);
+		//sscanf()
+		++count;
+	}
 }
